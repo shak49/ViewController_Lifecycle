77a269cd2899cf075422d278063dac38a13942c7
Shak49 <shakfeizi@gmail.com>
2022-07-18 20:56:22 -0500

    bug fixed

--- ViewController_Lifecycle/ViewController_Lifecycle/MainViewController.swift
+++ ViewController_Lifecycle/ViewController_Lifecycle/MainViewController.swift
@@ -12,19 +12,19 @@ class MainViewController: UIViewController {
     var _title: String = "Start free trial"
     var body: String = "3 days trial, then $1.99/mo."
     
-    var users: [User?] = []
-    
     private var customButton: CustomButton!
     private var _view: CustomView!
     private var tableView: UITableView!
 
+    var users: [User] = []
+    
     //MARK: - Lifecycles
     override func loadView() {
         super.loadView()
         // setting up the view for loading to memory
         //customButton = CustomButton(frame: CGRect(x: 0, y: 0, width: 250, height: 55))
         //_view = CustomView(frame: CGRect(x: 20, y: 475, width: 350, height: 350))
-        tableView = UITableView()
+        tableView = UITableView(frame: .zero)
         self.view = tableView
     }
     
@@ -52,8 +52,8 @@ class MainViewController: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
         tableView.register(CustomTableViewCell.self, forCellReuseIdentifier: "userCell")
-//        tableView.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height)
-//        tableView.center = view.center
+        tableView.frame = CGRect(x: 0, y: 0, width: view.frame.size.width, height: view.frame.size.height)
+        tableView.center = view.center
     }
     
     func setupUIElements() {
@@ -64,13 +64,16 @@ class MainViewController: UIViewController {
     }
     
     func loadData() {
-        NetworkService.shared.requestData { result in
-            switch result {
-            case .failure(let error):
-                print(error)
-            case .success(let user):
-                self.users.append(user)
-                //print(self.users)
+        NetworkService.shared.requestData { [weak self] (result) in
+            DispatchQueue.main.async {
+                switch result {
+                case .failure(let error):
+                    print("<<<-\(error)->>>")
+                case .success(let user):
+                    self?.users.append(user)
+                    //print(self?.users)
+                    self?.tableView.reloadData()
+                }
             }
         }
     }
@@ -78,18 +81,16 @@ class MainViewController: UIViewController {
 
 extension MainViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        print(users.count)
         return users.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "userCell", for: indexPath) as? CustomTableViewCell else { return UITableViewCell() }
         let user = users[indexPath.row]
-        cell.titleLable.text = user?.title
-        cell.bodyLable.text = user?.body
+        cell.titleLable.text = user.title
+        //cell.bodyLable.text = user.body
+        cell.selectionStyle = .none
         return cell
     }
-    
-    
 }
 
